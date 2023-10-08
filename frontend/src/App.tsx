3ea21d0789fed09ebc160926a7e6f48cb18c7d6b
Солтангазиева Мария Алексеевна <avemaryplus@gmail.com>
2023-10-08 22:39:42 +0600

    #1 добавлен роут для страницы с постом

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,10 @@ import "./App.css";
 import AppToolbar from "./components/UI/AppToolbar/AppToolbar";
 import { Route, Routes } from "react-router-dom";
 import RegisterPage from "./containers/RegisterPage/RegisterPage";
-<<<<<<< HEAD
 import PostsPage from "./containers/post/PostsPage";
 import PostDetailsPage from "./containers/post/PostDetailsPage";
-=======
 import LoginPage from "./containers/LoginPage/LoginPage";
->>>>>>> c2d4b97 (Пользователь может успешно войти в систему.)
 
-import PostsPage from "./containers/post/PostsPage";
-import PostDetailsPage from "./containers/post/PostDetailsPage";
 
 function App() {
   return (
@@ -23,7 +18,8 @@ function App() {
             <Route path="/" element={<PostsPage />} />
             <Route path="/posts/:id" element={<PostDetailsPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={"Login page here <RegisterPage/>"} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/posts/:id" element={<PostDetailsPage />} />
           </Routes>
         </Container>
       </main>
