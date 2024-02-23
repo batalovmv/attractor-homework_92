45e7b92f14472d436ab54801ed7bd04450885312
batalovmv <batalov94@gmail.com>
2024-02-23 23:58:31 +0600

    тест

--- frontend/src/components/UI/Menu/UserMenu.tsx
+++ frontend/src/components/UI/Menu/UserMenu.tsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 import { useAppDispatch } from "../../../store/hooks";
 import { logoutUser } from "../../../features/user/userSlice";
 import styled from "@emotion/styled";
+import { IconButton, Typography } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
 
 interface Props {
   username: string;
@@ -18,53 +20,59 @@ const StyledLink = styled(Link)(() => ({
 }));
 
 export default function UserMenu({ username }: Props) {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const dispatch = useAppDispatch();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const dispatch = useAppDispatch();
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleLogout = () => {
-    dispatch(logoutUser());
-  };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  return (
-    <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-      >
-        Hello {username}
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem onClick={handleClose}>
-          <Button component={StyledLink} to={"/add-post"}>
-            Add new post
-          </Button>
-        </MenuItem>
+    const handleLogout = () => {
+        handleClose(); 
+        dispatch(logoutUser());
+    };
 
-        <MenuItem onClick={handleLogout}>
-          <Button component={StyledLink} to={"/"}>
-            Logout
-          </Button>
-        </MenuItem>
-      </Menu>
-    </div>
-  );
+    return (
+        <div>
+            <IconButton
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                color="inherit"
+                size="large"
+            >
+                <AccountCircle />
+                <Typography variant="subtitle1" component="span">
+                    {username}
+                </Typography>
+            </IconButton>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <Button component={StyledLink} to={"/add-post"}>
+                        Add new post
+                    </Button>
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                    <Button component={StyledLink} to={"/"}>
+                        Logout
+                    </Button>
+                </MenuItem>
+            </Menu>
+        </div>
+    );
 }
