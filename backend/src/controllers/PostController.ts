51e63d4b857abcf801c71bbfc6fbae0c1ab4cba7
batalovmv <batalov94@gmail.com>
2024-02-23 14:37:48 +0600

    тест

--- backend/src/controllers/PostController.ts
+++ backend/src/controllers/PostController.ts
@@ -11,54 +11,46 @@ import { CommentRepository } from "../repositories/blogComment.repository";
 export class PostController {
 
 
-    // Для getAllPosts
     @Get('/')
     async getAllPosts(@CurrentUser() user?: User) {
         const currentUserId = user?.id || null;
 
         const posts = await PostRepository.createQueryBuilder("post")
             .leftJoinAndSelect("post.user", "user")
-            .leftJoin("post.comments", "comments")
-            .leftJoin("post.likes", "likes")
+            .leftJoinAndSelect("post.comments", "comments")
+            .leftJoinAndSelect("post.likes", "likes")
+            .loadRelationCountAndMap('post.commentCount', 'post.comments')
+            .loadRelationCountAndMap('post.likeCount', 'post.likes')
             .addSelect(subQuery => {
                 return subQuery
-                    .select("COUNT(DISTINCT comments.id)", "commentCount")
-                    .from("comment", "comments")
-                    .where("comments.postId = post.id");
-            }, "commentCount")
-            .addSelect(subQuery => {
-                return subQuery
-                    .select("COUNT(DISTINCT likes.id)", "likeCount")
-                    .from("like", "likes")
-                    .where("likes.postId = post.id");
-            }, "likeCount")
-            .addSelect(subQuery => {
-                return subQuery
-                    .select("CASE WHEN COUNT(likeUser) > 0 THEN true ELSE false END", "currentUserLiked")
-                    .from("like", "likeUser")
-                    .where("likeUser.postId = post.id")
-                    .andWhere("likeUser.userId = :currentUserId", { currentUserId });
+                    .select("count(like1.id)", "currentUserLiked")
+                    .from("like", "like1")
+                    .where("like1.postId = post.id AND like1.userId = :currentUserId", { currentUserId });
             }, "currentUserLiked")
+            .where("post.deleted = :deleted", { deleted: false })
             .groupBy("post.id")
             .addGroupBy("user.id")
             .orderBy("post.datetime", "DESC")
-            .setParameter("currentUserId", currentUserId)
-            .getRawMany();
+            .getRawAndEntities();
 
-        return posts.map(post => ({
-            id: post.post_id,
-            title: post.post_title,
-            description: post.post_description,
-            image: post.post_image,
-            datetime: post.post_datetime,
-            user: {
-                id: post.user_id,
-                username: post.user_username,
-            },
-            commentCount: Number(post.commentCount),
-            likeCount: Number(post.likeCount),
-            currentUserLiked: Number(post.currentUserLiked) > 0,
-        }));
+        return posts.entities.map((post, index) => {
+            const raw = posts.raw[index];
+            return {
+                id: post.id,
+                title: post.title,
+                description: post.description,
+                image: post.image,
+                datetime: post.datetime,
+                user: {
+                    id: post.user.id,
+                    username: post.user.username,
+                },
+                commentCount: raw.post_commentCount,
+                likeCount: raw.post_likeCount,
+                currentUserLiked: raw.currentUserLiked > 0
+            };
+        });
+    
     }
 
     // Для getPost
