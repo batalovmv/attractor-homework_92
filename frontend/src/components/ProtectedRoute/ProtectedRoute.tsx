bf7d0686eaba4676c55ef4e76b73d8f6d2f46476
batalovmv <batalov94@gmail.com>
2024-02-24 22:41:10 +0600

    test

--- frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,23 +1,30 @@
 import { Navigate} from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
+import { Box, CircularProgress } from "@mui/material";
 
 interface Props {
-  isAllowed?: boolean;
-  redirectPath: string;
-    children:any
+    isAllowed?: boolean;
+    redirectPath: string;
+    children: React.ReactNode; // Лучше использовать React.ReactNode для типизации детей
 }
 
-const ProtectedRoute = ({ isAllowed, redirectPath, children }:Props) => {
+const ProtectedRoute = ({ isAllowed, redirectPath, children }: Props) => {
     const authLoading = useAppSelector((state) => state.user.authLoading);
 
     if (authLoading) {
-        return <div>Loading...</div>; // Или индикатор загрузки, если у вас есть
+        // Центрируем индикатор загрузки на странице
+        return (
+            <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+                <CircularProgress />
+            </Box>
+        );
     }
 
     if (!isAllowed) {
         return <Navigate to={redirectPath} replace />;
     }
 
-    return children;
+    return <>{children}</>;
 };
+
 export default ProtectedRoute;
